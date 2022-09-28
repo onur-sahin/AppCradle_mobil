6430357f602acda46370a5d5355afbeda9fb3f66
github21yandex <ahmedgithub@yandex.com>
2022-09-28 16:59:37 -0700

    save

---
+++ __main__.py
@@ -0,0 +1,17 @@
+from kivy.app import App
+
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.core.window import Window
+from kivy.metrics import dp
+
+Window.size = (378, 672)
+
+class MainBoxLayout(BoxLayout):
+    pass
+
+class mainApp(App):
+    pass
+
+
+mainApp().run()
